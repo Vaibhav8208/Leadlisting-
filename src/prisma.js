c1bd1a1c3069b940d8c0a1266dfdfc33dee21bc0
Vikram Sanjay Lale <lalevikram32@gmail.com>
2025-07-23 21:47:26 +0530

    Delete src directory

--- src/prisma.js
+++
@@ -1,6 +0,0 @@
-// backend/src/prisma.js
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
-
-export default prisma;
--- src/routes/callLogsRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { createCallLog } from '../controllers/callLogController.js';
-
-const router = express.Router();
-
-router.post('/', createCallLog);
-
-export default router;
